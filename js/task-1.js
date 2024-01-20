e31fedb14e5d98c54fcef3bd0a6c90f808305147
Alexandr-1973 <144156771+Alexandr-1973@users.noreply.github.com>
2024-01-20 22:13:47 +0200

    start

--- js/task-1.js
+++
@@ -1,9 +0,0 @@
-const needCategories = document
-  .querySelector('ul#categories')
-  .querySelectorAll('.item');
-console.log(`Number of categories: ${needCategories.length}`);
-
-needCategories.forEach(elem => {
-  console.log(`Category: ${elem.querySelector('h2').textContent}`);
-  console.log(`Elements: ${elem.querySelectorAll('li').length}`);
-});
